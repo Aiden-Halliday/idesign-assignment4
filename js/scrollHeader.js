338cf1f09b54d4adb119b0b5c171fd18b3b1a62d
Aiden-Halliday <ashallid@lakeheadu.ca>
2025-03-25 21:31:01 -0400

    Add files via upload

--- js/scrollHeader.js
+++ js/scrollHeader.js
@@ -1,8 +1,10 @@
 $(window).on("scroll", function(){
     if($(this).scrollTop() > 600){
         $(".desktop-nav").addClass("scrolledHeader");
+        $(".mobile-nav").addClass("scrolledHeader");
     }
     else{
         $(".desktop-nav").removeClass("scrolledHeader");
+        $(".mobile-nav").removeClass("scrolledHeader");
     }
 });
